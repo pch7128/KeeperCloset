b2a3c8c7408ffc8bbc2e44b6d3e6778441a677e4
pch7128 <pch7128@naver.com>
2024-05-29 15:58:55 +0900

    예약DB 저장

--- KeeperCloset/src/main/java/com/pch7128/keepercloset/controller/RvController.java
+++ KeeperCloset/src/main/java/com/pch7128/keepercloset/controller/RvController.java
@@ -3,13 +3,20 @@ package com.pch7128.keepercloset.controller;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pch7128.keepercloset.dto.BoxSizeDTO;
+import com.pch7128.keepercloset.dto.PrincipalDetails;
+import com.pch7128.keepercloset.dto.Reservation;
 import com.pch7128.keepercloset.dto.StorageDTO;
+import com.pch7128.keepercloset.oauth.OAuth2UserInfo;
 import com.pch7128.keepercloset.repository.RvMapper;
 import com.pch7128.keepercloset.repository.RvRepository;
 
@@ -19,15 +26,27 @@ public class RvController {
 	
 	@Autowired
 	private RvMapper rvm;
+	@Autowired
+	private RvRepository rvre;
 	
 	@GetMapping("/keeping")
-	public String bookingPage(Model m) {
-			
+	public String bookingPage(@AuthenticationPrincipal PrincipalDetails principal,Model m) {
+		if(principal!=null) {
+			m.addAttribute("unum", principal.getMember().getUnum());
+		} else {
+			m.addAttribute("unum", "");
+		}
+
 		return "kc/rv/booking";
 	}
 	
 	
-	
+	@PostMapping("/keeping")
+	public String reservation(Reservation rv,Model m,@AuthenticationPrincipal PrincipalDetails principal) {
+		rvre.save(rv);
+		
+		return "kc/rv/bookingResult";
+	}
 	
 	
 
--- KeeperCloset/src/main/java/com/pch7128/keepercloset/dto/Reservation.java
+++ KeeperCloset/src/main/java/com/pch7128/keepercloset/dto/Reservation.java
@@ -3,7 +3,10 @@ package com.pch7128.keepercloset.dto;
 import org.springframework.stereotype.Component;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -12,15 +15,18 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 @Entity
+@Table(name="reservation")
 public class Reservation {
 	
 	@Id
+	@SequenceGenerator(sequenceName="SEQ_RVNUM", allocationSize=1, name="SEQ_RVNUM")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_RVNUM")
 	private int rvnum;
 	private int unum;
 	private String startdate;
 	private String enddate;
 	private String rv_name;
-	private String stnum;
+	private int stnum;
 	private String boxsize;
 	private int rv_price;
 	private int rv_cnt;
